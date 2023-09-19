9bf4c317fcf0e3bc2b17ddf525fe6a8c7d686fa7
Fanindra Nayak <fanindranayak@gmail.com>
2023-09-19 12:28:39 +0530

    Description added in linked list

--- Linked List/Singly Linked List/Initializing_LInked_List.cpp
+++ Linked List/Singly Linked List/Initializing_LInked_List.cpp
@@ -2,34 +2,41 @@
 
 using namespace std;
 
-class linked_list{
-    public:
-        int data;
-        linked_list *next;
+// Using class to create a singly linked list node
 
-        linked_list(int val){
-            data = val;
-            next=NULL;
-        }
+class linked_list
+{
+public:
+    int data;
+    linked_list *next;
+
+    linked_list(int val)
+    {
+        data = val;
+        next = NULL;
+    }
 };
 
-void print(linked_list *head){
+void print(linked_list *head)
+{
     linked_list *temp = head;
 
-    while(temp){
-        cout<<temp->data<<" ";
+    while (temp)
+    {
+        cout << temp->data << " ";
         temp = temp->next;
     }
 }
 
-int main(){
+int main()
+{
     linked_list *head = NULL;
 
     head = new linked_list(1);
 
-    head -> next = new linked_list(2);
+    head->next = new linked_list(2);
 
     print(head);
-    
+
     return 0;
 }
